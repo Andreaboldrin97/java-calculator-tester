1a6b47a8db6f5e8dc3e3d37c9a38eccbb709600d
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-11-24 17:07:32 +0100

    created the calculator methods

--- src/main/java/org/calculator/Calculator.java
+++ src/main/java/org/calculator/Calculator.java
@@ -2,4 +2,25 @@ package org.calculator;
 
 public class Calculator {
 
+	
+	//metodi di una calcolatrice
+	public float addition(float num1, float num2) {
+		
+		return num1 + num2;
+	}
+	
+	public float subtraction(float num1, float num2) {
+		
+		return num1 - num2;
+	}
+	
+	public float division(float num1, float num2) {
+		
+		return num1 / num2;
+	}
+	
+	public float multiplication(float num1, float num2) {
+		
+		return num1 * num2;
+	}
 }
